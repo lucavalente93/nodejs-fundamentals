741aa6b6109fa93b84b9712a1aeb8cf221458a86
Luca Valente <contact.lucamarins@gmail.com>
2025-10-13 14:36:21 -0300

    Studied request stream handling and JSON parsing
    - Implemented full stream reading to capture the request body
    - Parsed the request body into a JavaScript object
    - Used parsed data for creating users
    - Tested requests using Insomnia instead of httpie

--- 01-basic-server/src/server.js
+++ 01-basic-server/src/server.js
@@ -1,32 +1,50 @@
-import http from 'node:http';
+import http from 'node:http'
 
-const users = [];
+const users = []
 
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
   // métodos HTTP e URL são obtidas através do parâmetro REQUEST (req):
-  const method = req.method;
-  const url = req.url;
+  const method = req.method
+  const url = req.url
   //ou utilizando desestruturação
   // const {method, url} = req
 
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+  
+  // Se o corpo da requisição não existir, retorne nada.
+  try {
+    //Transformando o texto do body em JSON  
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+   // Se der erro (corpo vazio) não existir, não tenha body.
+  } catch {
+    req.body = null
+  }
+
+  console.log(req.body)
+  
   if (method === 'GET' && url === '/users'){
   // early return - omitindo else statement
     return res
     .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users));
+    .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users'){
+    const { name, email } = req.body
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@gmail.com"
-    });
-    return res.writeHead(201).end();
+      name, 
+      email,
+    })
+    return res.writeHead(201).end()
   }
 
-return res.writeHead(404).end();
-});
+return res.writeHead(404).end()
+})
 
-server.listen(9393);
+server.listen(9393)
